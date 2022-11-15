8eb5b6d658290f6acf1cdcdfc7bf2fc175cf9305
miracle-21 <93898332+miracle-21@users.noreply.github.com>
2022-11-15 11:06:56 +0900

    Update 완주하지 못한 선수.py

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,16 +1,12 @@
 def solution(participant, completion):
-    if len(participant) == len(set(participant)):
-        return (list(set(participant) - set(completion)))[0]
-    else:
-        dict = {}
-        for i in participant:
-            try:
-                dict[i] += 1
-            except:
-                dict[i] = 1
-        for i in completion:
-            dict[i] -= 1
-        for key, value in dict.items():
-            if value != 0:
-                return key
-        
+    dict = {}
+    for i in participant:
+        try:
+            dict[i] += 1
+        except:
+            dict[i] = 1
+    for i in completion:
+        dict[i] -= 1
+    for key, value in dict.items():
+        if value != 0:
+            return key
