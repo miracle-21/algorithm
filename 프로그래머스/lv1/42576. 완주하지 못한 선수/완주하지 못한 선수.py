6347e5d6455c48f5fd6acbd9c583860080b152c7
miracle-21 <93898332+miracle-21@users.noreply.github.com>
2022-11-15 11:04:56 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 61.90 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,23 +1,16 @@
 def solution(participant, completion):
-    dict = {}
-    for i in participant:
-        try:
-            dict[i] += 1
-        except:
-            dict[i] = 1
-    for i in completion:
-        dict[i] -= 1
-    for key, value in dict.items():
-        if value != 0:
-            return key
-    
-    # 1. 효율성 테스트 0%
-    # for i in participant:
-    #     if participant.count(i) != completion.count(i):
-    #         return i
-    
-    # 2. 효율성 테스트 0%
-    # lst = participant+completion
-    # for i in lst:
-    #     if lst.count(i) % 2 == 1:
-    #         return i
+    if len(participant) == len(set(participant)):
+        return (list(set(participant) - set(completion)))[0]
+    else:
+        dict = {}
+        for i in participant:
+            try:
+                dict[i] += 1
+            except:
+                dict[i] = 1
+        for i in completion:
+            dict[i] -= 1
+        for key, value in dict.items():
+            if value != 0:
+                return key
+        
