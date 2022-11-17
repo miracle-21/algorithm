d29b29f045aea1001fd6de6ac5a34ee25d929d9a
miracle-21 <93898332+miracle-21@users.noreply.github.com>
2022-11-17 12:57:47 +0900

    [level 2] Title: 가장 큰 수, Time: 0.02 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 def solution(numbers):
     lst = []
     answer = ''
@@ -9,17 +7,3 @@ def solution(numbers):
     for i in lst:
         answer += i
     return str(int(answer))
-    # 정답률 53.3
-    # lst = []
-    # for i in numbers:
-    #     lst.append(str(i))
-    # lst.sort(reverse=True)
-    # q = deque(lst)
-    # answer = ''
-    # while len(q) > 1:
-    #     if q[0]+q[1] > q[1]+q[0]:
-    #         answer += q.popleft()
-    #     else:
-    #         answer += q[1]
-    #         q.remove(q[1])
-    # return answer + q[-1]
