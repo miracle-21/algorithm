a1a0a2c342160543f923839d7e71c85608f752c1
miracle-21 <93898332+miracle-21@users.noreply.github.com>
2023-01-04 14:59:20 +0900

    [level 1] Title: 소수 찾기, Time: 3098.37 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
@@ -0,0 +1,11 @@
+import math
+
+def solution(n):
+    result = 0
+    for i in range(2,n+1):
+        for j in range(2, int(math.sqrt(i))+1):
+            if i % j == 0:
+                break
+        else:
+            result += 1
+    return result
